0bd498321d3814e774193a56890484f5a9ffcd69
Alemas <mateusreckziegel@gmail.com>
2018-08-29 18:55:24 -0300

    Exercise 6

--- ex5.py
+++ ex5.py
@@ -2,6 +2,6 @@ my_name = 'Mateus'
 my_age = 23
 my_eyes = 'blue'
 
-#O 'f' na frente da string indica que ela vai receber variaveis para serem concatencadas {my_name}
+#The 'f' indicates that the string may receive values from variables. In this case {my_name}
 print(f"Let's talk about {my_name}")
 print(f"He's {my_age} years old and\nhas {my_eyes} eyes")
---
+++ ex6.py
@@ -0,0 +1,23 @@
+types_of_people = 10
+x = f"There are {types_of_people} types of people"
+
+binary = "binary"
+do_not = "don't"
+
+y = f"Those who know {binary} and those who {do_not}"
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+hilarious = False
+joke_evaluation = "Isn't that funny? {}"
+
+print(joke_evaluation.format(hilarious))
+
+left_side = "This is the left side of ... "
+right_side = "a string with a right side"
+
+print(left_side + right_side)
