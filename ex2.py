fb135e04b37dcafea22a9a40a6c6f6bf3c786689
Alemas <mateusreckziegel@gmail.com>
2018-08-29 18:20:44 -0300

    Added first exercises

---
+++ ex2.py
@@ -0,0 +1,4 @@
+# This is a comment
+# Comments can be written after code in the same line
+
+print"This is not commented" # ,but this is
---
+++ ex5.py
@@ -0,0 +1,7 @@
+my_name = 'Mateus'
+my_age = 23
+my_eyes = 'blue'
+
+#O 'f' na frente da string indica que ela vai receber variaveis para serem concatencadas {my_name}
+print(f"Let's talk about {my_name}")
+print(f"He's {my_age} years old and\nhas {my_eyes} eyes")
