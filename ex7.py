33ef1a785220aa4a14f84304dff801dd1e2845b8
Alemas <mateusreckziegel@gmail.com>
2018-08-29 19:03:42 -0300

    Exercise 7

---
+++ ex7.py
@@ -0,0 +1,17 @@
+print("Mary had a little lamb.")
+print("Its fleece was whtie as {}.".format('snow'))
+print("And everywhere that Mary went.")
+#The * operator can be applied to strings
+print("." * 10)
+
+end1 = "A"
+end2 = "p"
+end3 = "p"
+end4 = "l"
+end5 = "e"
+end6 = "p"
+end7 = "i"
+end8 = "e"
+
+print(end1 + end2 + end3 + end4 + end5, end = ' ')
+print(end6 + end7 + end8)
